2548accbb8b7fec7a3a3bc59d8df2925f0150902
Elma-dev <67378945+Elma-dev@users.noreply.github.com>
2023-09-18 15:39:37 +0100

    commit

--- customer-service/src/main/java/dev/elma/customerservice/web/CustomerSoapController.java
+++ customer-service/src/main/java/dev/elma/customerservice/web/CustomerSoapController.java
@@ -1,5 +1,35 @@
 package dev.elma.customerservice.web;
 
+import dev.elma.customerservice.dto.CustomerRequest;
+import dev.elma.customerservice.entities.Customer;
+import dev.elma.customerservice.mappers.CustomerMapper;
+import dev.elma.customerservice.repositories.CustomerRepository;
+import jakarta.jws.WebMethod;
+import jakarta.jws.WebParam;
+import jakarta.jws.WebService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@WebService(name = "CustomerSoapWS")
+@AllArgsConstructor
 public class CustomerSoapController {
+    private CustomerRepository customerRepository;
+
+    @WebMethod
+    public List<Customer> getAllCustomers(){
+        return customerRepository.findAll();
+    }
+    @WebMethod
+    public Customer getCustomerById(@WebParam Long id){
+        return customerRepository.findById(id).get();
+    }
+
+    @WebMethod
+    public Customer saveCustomer(@WebParam CustomerRequest customerRequest){
+        return customerRepository.save(CustomerMapper.customerRequestToCustomer(customerRequest));
+    }
 
 }
