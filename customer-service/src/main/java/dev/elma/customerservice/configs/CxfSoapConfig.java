bd194a28b89f9eec54f6b58bba27328fc87c2ffd
Elma-dev <67378945+Elma-dev@users.noreply.github.com>
2023-09-18 15:44:49 +0100

    publishSoapController

--- customer-service/src/main/java/dev/elma/customerservice/configs/CxfSoapConfig.java
+++ customer-service/src/main/java/dev/elma/customerservice/configs/CxfSoapConfig.java
@@ -1,2 +1,22 @@
-package dev.elma.customerservice.configs;public class CxfSoapConfig {
+package dev.elma.customerservice.configs;
+
+import dev.elma.customerservice.web.CustomerSoapController;
+import lombok.AllArgsConstructor;
+import org.apache.cxf.Bus;
+import org.apache.cxf.jaxws.EndpointImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@AllArgsConstructor
+public class CxfSoapConfig {
+    private Bus bus;
+    private CustomerSoapController customerSoapController;
+
+    @Bean
+    public EndpointImpl endpoint(){
+        EndpointImpl endpoint=new EndpointImpl(bus,customerSoapController);
+        endpoint.publish("/customerService");
+        return endpoint;
+    }
 }
